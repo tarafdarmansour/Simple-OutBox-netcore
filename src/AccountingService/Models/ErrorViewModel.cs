c5a1e45a7c4ce07b4ad72d8c9168f0fd75a915e1
منصور طرفدار <m.tarafdar@faradis.net>
2021-06-13 08:17:20 +0430

    add solution and project files

---
+++ src/AccountingService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AccountingService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
